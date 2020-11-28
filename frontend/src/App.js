ae95493bb5611f843c6da2134b62cb6edbb394f0
danubiobwm <danubio.bwm@gmail.com>
2020-11-28 18:19:37 -0300

    frontend finish

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import './App.css';
 import Header from "./components/Header";
+import api from './services/api';
 
 function App() {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(()=>{
+    api.get('projects').then(response=>{
+     setProjects(response.data);
+    })
+  },[]);
+
+  async function handleAddProject(){
+   /*  setProjects([...projects,`Novo projeto ${Date.now()}` ]) */
+   const response = await api.post('projects', {
+    title:`Novo projeto ${Date.now()}`,
+    owner:"Danubio"
+   });
+   const project=response.data;
+   setProjects([...projects, projects])
+  }
+
+
   return (
-    <>
-      <Header title="Header 1">
-        <ul>
-          <li>Home</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Header 2"/>
-    </>
-  );
+  <>
+  <Header title="Projects" />
+
+  <ul>
+    {projects.map(project =><li key={project.id}> {project.title}</li>)}
+  </ul>
+  <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+  </>
+  )
 }
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-// import { Container } from './styles';
-
-export default function Header({title, children}) {
-  return(
+export default function Header({ title, children }) {
+  return (
     <header>
       <h1>{title}</h1>
-      {children}
     </header>
   )
 }
-
