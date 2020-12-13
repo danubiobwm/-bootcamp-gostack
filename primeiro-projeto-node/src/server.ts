6da01e4042d5e75dd3b30db32b93cfb393aab867
danubiobwm <danubio.bwm@gmail.com>
2020-12-13 01:10:56 -0300

    init node and typescript

---
+++ primeiro-projeto-node/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333,()=>{
+  console.log('💥️🐍️💻️Server Started on port 3333💥️🐍️💻️');
+})
