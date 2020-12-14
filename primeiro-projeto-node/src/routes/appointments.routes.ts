b8cf52910d79eb697838cb32ae64a65d67f4c1eb
danubiobwm <danubio.bwm@gmail.com>
2020-12-14 20:10:33 -0300

    Validating data

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,19 +1,42 @@
 import { response, Router } from 'express';
 import { uuid } from 'uuidv4';
+import { parseISO, startOfHour, isEqual } from 'date-fns'
+
+interface IAppointment {
+  id: string;
+  provider: string;
+  date: Date;
+
+}
+
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: IAppointment[] = [];
 
 appointmentsRouter.post('/', (resquest, response) => {
   const { provider, date } = resquest.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'this appointment is alreadt booked' })
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   }
-appointments.push(appointment)
+  appointments.push(appointment)
 
   return response.json(appointment);
 })
 
 export default appointmentsRouter;
+
+
