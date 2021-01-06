228f57ff14f3210821ea0c8dce8e015ac054b7c2
danubiobwm <danubio.bwm@gmail.com>
2021-01-06 19:34:48 -0300

    feat: :art: Isolating Components
    Isolating Components
    
    Isolating Components

--- 06-gobarber-web/src/pages/SignIn/index.tsx
+++ 06-gobarber-web/src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import Logo from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
@@ -11,9 +13,14 @@ const SignIn: React.FC = () => {
           <img src={Logo} alt="GoBarber" />
           <form>
             <h1> Faça seu logo</h1>
-            <input placeholder="E-mail" />
-            <input type="password" placeholder="Senha" />
-            <button type="submit">Entrar</button>
+            <Input name="E-mail" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="Senha"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+            <Button type="submit">Entrar</Button>
             <a href="forgot">Esqueci minha senha</a>
           </form>
           <a href="forgot">
