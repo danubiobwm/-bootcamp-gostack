dc26f24fc29d59d5d4172de1b43d6985f5937a7d
danubiobwm <danubio.bwm@gmail.com>
2021-02-26 20:04:55 -0300

    feat: :card_file_box: feature/InputButton
    feature/InputButton
    
    feature/InputButton

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
- import { Container } from './styles';
+const SignIn: React.FC = () => {
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+        contentContainerStyle={{ flex:1}}
+        keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu Cadastro</Title>
+            </View>
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senhar" />
+            <Button
+              onPress={() => {
+                console.log('oi');
+              }}
+            >
+              Entrar
+            </Button>
 
-const SignUp: React.FC = () => {
-  return <Container />;
-}
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText> Criar uma Conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
+  );
+};
 
-export default SignUp;
+export default SignIn;
