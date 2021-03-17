00100f4843afd88b0febe77f476d109741cbc5bc
danubiobwm <danubio.bwm@gmail.com>
2021-03-17 00:27:44 -0300

    feat: :bug: Liskov Substitution Principle / Config Imports
    Liskov Substitution Principle / Config Imports
    
    Liskov Substitution Principle / Config Imports

--- iniciando-backend/src/shared/infra/http/server.ts
+++ iniciando-backend/src/shared/infra/http/server.ts
@@ -3,11 +3,11 @@ import express, { Request, NextFunction, Response } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
 import routes from './routes';
-import uploadConfig from '../../../config/upload';
-import AppError from '../../errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 app.use(cors());
