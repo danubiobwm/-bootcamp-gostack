5b9723a837cfa30bfb950eee0f30f1ae9b4989a4
danubiobwm <danubio.bwm@gmail.com>
2021-03-18 01:10:13 -0300

    feat: :fire: Dependency Inversion Principle
    Dependency Inversion Principle
    
    Dependency Inversion Principle

--- iniciando-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ iniciando-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,28 +1,27 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 
+const appointmentsRepository = new AppointmentsRepository();
+
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (resquest, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-
+/* appointmentsRouter.get('/', async (resquest, response) => {
   const appointments = await appointmentsRepository.find();
   return response.json(appointments);
-});
+}); */
 
 appointmentsRouter.post('/', async (resquest, response) => {
   const { provider_id, date } = resquest.body;
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(appointmentsRepository);
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
