21169ed7931f9cb451fcd45c84b3afc21f52aa09
danubiobwm <danubio.bwm@gmail.com>
2021-03-20 01:12:52 -0300

    feat: :ambulance: Refactoring Users Module
    Refactoring Users Module
    
    Refactoring Users Module

--- iniciando-backend/src/modules/users/infra/http/routes/users.routes.ts
+++ iniciando-backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository'
+
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -13,7 +15,9 @@ const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (resquest, response) => {
   const { name, email, password } = resquest.body;
-  const createUser = new CreateUserService();
+
+  const userRepository = new UsersRepository();
+  const createUser = new CreateUserService(userRepository);
 
   const user = await createUser.execute({
     name,
@@ -37,7 +41,8 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
+    const userRepository = new UsersRepository();
+    const updateUserAvatar = new UpdateUserAvatarService(userRepository);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
