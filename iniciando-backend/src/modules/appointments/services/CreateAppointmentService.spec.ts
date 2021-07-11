cbf5dc5e7b7d3fccab515eef2e7e4f1f8b5ea5b6
danubiobwm <danubio.bwm@gmail.com>
2021-07-11 01:32:28 -0300

    feat: :bulb: Configuring MongoD
    Configuring MongoD
    
    Configuring MongoD

--- iniciando-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ iniciando-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,29 +1,36 @@
 import 'reflect-metadata';
-import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
-import CreateAppointmentService from './CreateAppointmentService';
 
 import AppError from '@shared/errors/AppError';
 
-let createAppointment: CreateAppointmentService;
+import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let fakeNotificationsRepository: FakeNotificationsRepository;
+let createAppointment: CreateAppointmentService;
 
 describe('CreateAppointment', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    fakeNotificationsRepository = new FakeNotificationsRepository();
+
     createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
+      fakeNotificationsRepository
     );
   });
 
   it('should be able to create a new appointment', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2021, 4, 10, 12).getTime();
+      return new Date(2021, 8, 10, 12).getTime();
     });
 
     const appointment = await createAppointment.execute({
-      date: new Date(2021, 4, 10, 13),
-      provider_id: 'provider-id',
+      date: new Date(2021, 8, 10, 13),
       user_id: 'user-id',
+      provider_id: 'provider-id',
     });
 
     expect(appointment).toHaveProperty('id');
@@ -31,7 +38,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date(2021, 6, 10, 11);
+    const appointmentDate = new Date(2021, 8, 10, 11);
 
     await createAppointment.execute({
       date: appointmentDate,
@@ -50,12 +57,12 @@ describe('CreateAppointment', () => {
 
   it('should not be able to create an appointment on a past date', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2021, 4, 10, 12).getTime();
+      return new Date(2021, 8, 10, 12).getTime();
     });
 
     await expect(
       createAppointment.execute({
-        date: new Date(2021, 4, 10, 11),
+        date: new Date(2021, 8, 10, 11),
         provider_id: 'provider-id',
         user_id: 'user-id',
       }),
@@ -64,12 +71,12 @@ describe('CreateAppointment', () => {
 
   it('should not be able to create an appointment with same user as provider', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2021, 4, 10, 12).getTime();
+      return new Date(2021, 8, 10, 12).getTime();
     });
 
     await expect(
       createAppointment.execute({
-        date: new Date(2021, 4, 10, 13),
+        date: new Date(2021, 8, 10, 13),
         provider_id: 'user-id',
         user_id: 'user-id',
       }),
@@ -78,12 +85,12 @@ describe('CreateAppointment', () => {
 
   it('should not be able to create an appointment before 8am and after 5pm', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2021, 4, 10, 12).getTime();
+      return new Date(2021, 8, 10, 12).getTime();
     });
 
     await expect(
       createAppointment.execute({
-        date: new Date(2021, 4, 11, 7),
+        date: new Date(2021, 8, 11, 7),
         provider_id: 'user-id',
         user_id: 'provider-id',
       }),
@@ -91,7 +98,7 @@ describe('CreateAppointment', () => {
 
     await expect(
       createAppointment.execute({
-        date: new Date(2021, 4, 11, 18),
+        date: new Date(2021, 8, 11, 18),
         provider_id: 'user-id',
         user_id: 'provider-id',
       }),
